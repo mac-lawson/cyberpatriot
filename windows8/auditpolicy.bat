9876c8c49a356ad9c4f94ac1178463972c676d79
mac lawson <macattackairman@gmail.com>
2023-05-24 19:01:22 -0400

    windows 8

--- windows8/auditpolicy.bat
+++ windows8/auditpolicy.bat
@@ -3,41 +3,45 @@
 REM Author: Mac Lawson
 REM License: GPLv3
 
-REM Disable unnecessary services
-
 REM Enable auditing for account logon events
-auditpol /set /subcategory:"Account Logon" /success:enable /failure:enable
+echo Enabling auditing for account logon events...
+auditpol.exe /set /subcategory:"Account Logon" /success:enable /failure:enable
 
 REM Enable auditing for account management
-auditpol /set /subcategory:"Account Management" /success:enable /failure:enable
+echo Enabling auditing for account management...
+auditpol.exe /set /subcategory:"Account Management" /success:enable /failure:enable
 
 REM Enable auditing for directory service access
-auditpol /set /subcategory:"Directory Service Access" /success:enable /failure:enable
+echo Enabling auditing for directory service access...
+auditpol.exe /set /subcategory:"Directory Service Access" /success:enable /failure:enable
 
 REM Enable auditing for logon events
-auditpol /set /subcategory:"Logon" /success:enable /failure:enable
+echo Enabling auditing for logon events...
+auditpol.exe /set /subcategory:"Logon" /success:enable /failure:enable
 
 REM Enable auditing for object access
-auditpol /set /subcategory:"Object Access" /success:enable /failure:enable
+echo Enabling auditing for object access...
+auditpol.exe /set /subcategory:"Object Access" /success:enable /failure:enable
 
 REM Enable auditing for privilege use
-auditpol /set /subcategory:"Privilege Use" /success:enable /failure:enable
+echo Enabling auditing for privilege use...
+auditpol.exe /set /subcategory:"Privilege Use" /success:enable /failure:enable
 
 REM Enable auditing for process tracking
-auditpol /set /subcategory:"Process Tracking" /success:enable /failure:enable
+echo Enabling auditing for process tracking...
+auditpol.exe /set /subcategory:"Process Tracking" /success:enable /failure:enable
 
 REM Enable auditing for system events
-auditpol /set /subcategory:"System" /success:enable /failure:enable
-
-REM Apply the new audit policies
-gpupdate /force
+echo Enabling auditing for system events...
+auditpol.exe /set /subcategory:"System" /success:enable /failure:enable
 
 REM Display the updated audit policies
-auditpol /get /subcategory:"Account Logon"
-auditpol /get /subcategory:"Account Management"
-auditpol /get /subcategory:"Directory Service Access"
-auditpol /get /subcategory:"Logon"
-auditpol /get /subcategory:"Object Access"
-auditpol /get /subcategory:"Privilege Use"
-auditpol /get /subcategory:"Process Tracking"
-auditpol /get /subcategory:"System"
+echo Displaying the updated audit policies...
+auditpol.exe /get /subcategory:"Account Logon"
+auditpol.exe /get /subcategory:"Account Management"
+auditpol.exe /get /subcategory:"Directory Service Access"
+auditpol.exe /get /subcategory:"Logon"
+auditpol.exe /get /subcategory:"Object Access"
+auditpol.exe /get /subcategory:"Privilege Use"
+auditpol.exe /get /subcategory:"Process Tracking"
+auditpol.exe /get /subcategory:"System"
